538575d4a496ada4d2bcc3d4991d81dcff3734da
okipriyadi <oki.priyadi@pacificavionics.net>
2015-09-16 16:36:33 +0700

    test

--- SamplePython/Developer Tool/fab_fabric/10_defining_connection_before_hand.py
+++ SamplePython/Developer Tool/fab_fabric/10_defining_connection_before_hand.py
@@ -1,9 +1,9 @@
 """
 Specifying connection info at runtime gets old real fast, so Fabric provides a handful 
-of ways to do it in your fabfile or on the command line. We won’t cover all of them here, 
+of ways to do it in your fabfile or on the command line. We won't cover all of them here, 
 but we will show you the most common one: setting the global host list, env.hosts.
 
-env is a global dictionary-like object driving many of Fabric’s settings, 
+env is a global dictionary-like object driving many of Fabric's settings, 
 and can be written to with attributes as well (in fact, settings, seen above, 
 is simply a wrapper for this.) Thus, we can modify it at module level near the 
 top of our fabfile like so:
@@ -12,10 +12,10 @@ from __future__ import with_statement
 from fabric.api import *
 from fabric.contrib.console import confirm
 
-env.hosts = ['my_server']
-
+env.hosts = '172.16.191.41'
+env.user = 'avionics'
 def test():
-    do_test_stuff()
+    result = run("ls ~")
     
 """
 When fab loads up our fabfile, our modification of env will execute, storing our settings change. The end result is exactly as above: our deploy task will run against the my_server server.
--- SamplePython/Developer Tool/fab_fabric/environtment/01_Pendahuluan.py
+++ SamplePython/Developer Tool/fab_fabric/environtment/01_Pendahuluan.py
@@ -3,7 +3,8 @@ A simple but integral aspect of Fabric is what is known as the “environment”
 a Python dictionary subclass, which is used as a combination settings registry and 
 shared inter-task data namespace.
 
-The environment dict is currently implemented as a global singleton, fabric.state.env, and is included in fabric.api for convenience. Keys in env are sometimes referred to as “env variables”.
-
-
+The environment dict is currently implemented as a global singleton, 
+fabric.state.env, and is included in fabric.api for convenience. 
+Keys in env are sometimes referred to as “env variables”.
 """
+
